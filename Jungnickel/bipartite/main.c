334363de0281592d7b6385fc786b784ce46e6370
hiratannnnnn <tomoya.hirata@wing.ocn.ne.jp>
2025-06-07 23:44:42 +0900

    asdf

--- Jungnickel/bipartite/main.c
+++ Jungnickel/bipartite/main.c
@@ -15,25 +15,16 @@ int main(void)
 	double **matrix;
 	int *match_to;
 	int a, b;
-	a = 50;
-	b = 109;
 
-	matrix = generate_random_bigraph_cost(a, b);
+	matrix = read_double_matrix(&a, &b, "b5.txt");
 	// print_matrix_double(matrix, a, b);
-	write_double_matrix(matrix, a, b, "b1.txt");
 	match_to = (int *)xcalloc(b, sizeof(int));
 	printf("%f\n", hungarian(matrix, a, b, match_to));
 	print_array_int(match_to, b);
+
 	// free
 	free_matrix_double(matrix, a, b);
 	free_array_int(match_to, b);
-
-	// double **matrix;
-
-	// a =
-
-
-
 	proc_end = clock();
 	print_info();
 	return (0);

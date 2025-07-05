0dc0093226bcb237e9a3fe2dcbd7dbf87d7bb10c
hiratannnnnn <tomoya.hirata@wing.ocn.ne.jp>
2025-07-06 00:46:28 +0900

    Edge->cost

--- Jungnickel/include/types.h
+++ Jungnickel/include/types.h
@@ -8,7 +8,7 @@ typedef struct 	Edge
     int         id;			// edge_id
     int         from;   	// vertex from
     int         to;     	// vertex to
-    int         used;		// for any extra uses
+    int         cost;		// cost
     struct Edge *next;		// for edge list
 }               Edge;
 
